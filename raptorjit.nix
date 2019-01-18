33d88ea39191082b60c171989b62ea68aa140e02
Luke Gorrie <luke@snabb.co>
2019-01-18 11:32:34 +0000

    raptorjit.nix: Check installation of bin/raptorjit and lib/raptorjit-5.1.so

--- raptorjit.nix
+++ raptorjit.nix
@@ -18,7 +18,14 @@ mkDerivation rec {
   installPhase = ''
     make install PREFIX="$out"
   '';
-
+  # Simple inventory test.
+  installCheckPhase = ''
+    for file in bin/raptorjit lib/libraptorjit-5.1.so; do
+      echo "Checking for $file"
+      test -f $out/$file
+    done
+  '';
+  doInstallCheck = true;
   enableParallelBuilding = true;  # Do 'make -j'
 }
 
