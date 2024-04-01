17fdbc21206afab39603f4051ce169761777d783
Max Rottenkolber <max@mr.gy>
2024-04-01 13:49:09 +0200

    Revert "Merge branch 'sync-luajit-v2.1-aug-2023' into next"
    This reverts commit 19a7db4f95318b6d3c75c919042c424e90ed1de9, reversing
    changes made to 334b69e2a7045bb705feb3c506f356b03193f283.

--- src/lj_obj.h
+++ src/lj_obj.h
@@ -812,7 +812,9 @@ static LJ_AINLINE int32_t lj_num2bit(lua_Number n)
 */
 static LJ_AINLINE uint64_t lj_num2u64(lua_Number n)
 {
-  return (uint64_t)n;
+  int64_t i = (int64_t)n;
+  if (i < 0) i = (int64_t)(n - 18446744073709551616.0);
+  return (uint64_t)i;
 }
 
 static LJ_AINLINE int32_t numberVint(cTValue *o)
